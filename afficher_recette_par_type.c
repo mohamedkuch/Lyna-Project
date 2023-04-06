08abb552fa1439340f8cc2fef8e561829ffde4a5
Mohamed Kechaou <mohamed.kechaou@alumni.fh-aachen.de>
2023-04-06 15:54:24 +0200

    finish first logic

--- afficher_recette_par_type.c
+++ afficher_recette_par_type.c
@@ -28,17 +28,18 @@ void lire_fichier_type(char nom[])
 void Afficher_recette_list_par_type(int type)
 {
     memset(liste_de_recette, 0, sizeof(liste_de_recette));
+    num_recettes = 0;
+
     char *token, *saveptr;
     int recette_type;
     char name[100];
-    char recette_string[100];
 
     DIR *dir;
     struct dirent *ent;
     char path[100] = "recettes/";
 
     printf("Quel nom de recette cherchez-vous ? \n");
-    
+
     if ((dir = opendir(path)) != NULL)
     {
         while ((ent = readdir(dir)) != NULL)
@@ -54,12 +55,9 @@ void Afficher_recette_list_par_type(int type)
                 token = strtok_r(NULL, "_", &saveptr);
                 sscanf(token, "%d", &recette_type);
 
-                // combine recette_type and name into a single string example : 1_buratta
-                sprintf(recette_string, "%d_%s", recette_type, name);
-
                 if (type == recette_type)
                 {
-                    strcpy(liste_de_recette[num_recettes], recette_string);
+                    strcpy(liste_de_recette[num_recettes], name);
                     num_recettes++;
 
                     printf("%d.%s\n", num_recettes, name);
@@ -72,59 +70,37 @@ void Afficher_recette_list_par_type(int type)
 
 void Afficher_recette_par_type(int type, int num)
 {
-
-    // print out the array
+    char recette[100];
     for (int i = 0; i < num_recettes; i++)
     {
-        printf("%s\n", liste_de_recette[i]);
-    }
-
-    if (type == 1)
-    {
-        if (num == 1)
+        if (i == num)
         {
-            /*Afficher le fichier recette de buratta*/
-            // lire_fichier_type("recettes/recette_buratta.txt");
-        }
-        if (num == 2)
-        {
-            /*Afficher le fichier recette de risotto*/
-        }
-        if (num == 3)
-        {
-            /*Afficher le fichier recette de linguine*/
+            strcpy(recette, liste_de_recette[i]);
+            break;
         }
     }
 
-    if (type == 2)
-    {
-        if (num == 1)
-        {
-            /*Afficher le fichier recette 1*/
-        }
-        if (num == 2)
-        {
-            /*Afficher le fichier recette 2*/
-        }
-        if (num == 3)
-        {
-            /*Afficher le fichier recette 3*/
-        }
-    }
+    DIR *dir;
+    struct dirent *ent;
+    char path[100] = "recettes/";
 
-    if (type == 3)
+    if ((dir = opendir(path)) != NULL)
     {
-        if (num == 1)
+        while ((ent = readdir(dir)) != NULL)
         {
-            /*Afficher le fichier recette 1*/
+            if (ent->d_type == DT_REG && strstr(ent->d_name, ".txt"))
+            {
+                char *result = strstr(ent->d_name, recette);
+
+                if (result != NULL)
+                {
+                    char recette_path[100];
+                    // combiner recettes/ avec nom de fichier de recette
+                    sprintf(recette_path, "%s/%s", "recettes", ent->d_name);
+                    lire_fichier_type(recette_path);
+                }
+            }
         }
-        if (num == 2)
-        {
-            /*Afficher le fichier recette 2*/
-        }
-        if (num == 3)
-        {
-            /*Afficher le fichier recette 3*/
-        }
+        closedir(dir);
     }
 }
